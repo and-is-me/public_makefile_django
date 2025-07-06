b5480ac44d095dbcfc477db2674df299ca33737b
AndreyKusko <dev.stuff.andy@gmail.com>
2025-07-07 00:47:56 +0300

    💾

--- project.mk
+++ project.mk
@@ -24,13 +24,13 @@ style_check:	## - check style
 	echo "run style ruff  check"
 	. $(ACTIVATE); ruff check
 style:		## - format style
-	echo "run style ruff format"
-	. $(ACTIVATE); ruff format
+	@echo "run style ruff format"
+	. $(ACTIVATE); @ruff format
 migrations:	## - create migrations
-	. $(ACTIVATE); src/manage.py makemigrations;
+	. $(ACTIVATE); @src/manage.py makemigrations;
 mig: migrations
 migrate: 	## - run migrations
-	. $(ACTIVATE); src/manage.py migrate
+	. $(ACTIVATE); @src/manage.py migrate
 m: migrate
 collect: 	## - collect static
 	. $(ACTIVATE); src/manage.py collectstatic --no-input
@@ -40,3 +40,11 @@ json_dumb: 	## - make dump
 
 run:		## - run server
 	. $(ACTIVATE); src/manage.py runserver 18040
+
+
+
+copy_gitignore:		## - run server
+	git fetch git@github.com:and-is-me/public_gitignore.git main:public_gitignore
+	git checkout public_gitignore -- .gitignore
+	git commit -m "Grabbed .gitignore from public_gitignore.git"
+	git push
